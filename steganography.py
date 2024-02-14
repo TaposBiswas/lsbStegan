3e2ec6700af97e352d03b62c171f5b12443d88a9
Tapos <48724151+TaposBiswas@users.noreply.github.com>
2024-02-15 01:25:04 +0600

    Update steganography.py
    This Python script simplifies the provided code for LSB steganography with PNG images. It includes two functions, embed_message and reveal_message, which respectively embed a message in an image and extract a message from an image. The script uses the argparse module to take command-line arguments, including the image file, message, and password. The script also includes error messages to help users understand how to use the script correctly. Overall, this script provides a more user-friendly implementation of LSB steganography for PNG images.

--- steganography.py
+++ steganography.py
@@ -1,64 +1,53 @@
 #!/usr/bin/python3
 
-from stegano import lsb
 import argparse
 import sys
 import random
 import re
 
-parser = argparse.ArgumentParser(description="Image Steganography\n[+] Use .png files only")
-parser.add_argument('-e',type=str,help="To embed a message in the image")
-parser.add_argument('-f',help="To pass .png (Image) file")
-parser.add_argument('-x',help="To extract the message from the image.",action="store_true")
-parser.add_argument('-p',help="To put in a password.", nargs="?", const="no_password_given")
-args = parser.parse_args()
+def embed_message(image_file, message, password):
+    if not image_file.endswith(".png"):
+        print("[-] Enter a .png file")
+        sys.exit(1)
 
-#./stego -f <filename.png> -e "secret_message" -p <Password (Optional)>" -> To embed a secret message.
-#./stego -f <secretfile.png> -x -p <Password supplied> -> To extract the secret message.
-#Note : Even if you passed a blank field in the -p argument,while creating a secret file, you must append a -p while extracting the message. 
-try:
-	if not args.f:
-		print("Please enter a file name.")
-		print("[*] Type -h for help.")
-		sys.exit(1)
-	
-	else:
-		if args.f and args.e:
-			if (".png" in args.f):
-				if not args.p:
-					args.p = "no_password_given"
-				embed = args.p +" "+ args.e
-				secret = lsb.hide(args.f,embed)
-				random = random.randint(0,100)
-				filename = "secret"+str(random)+".png"
-				secret.save(filename)
-				print("File Saved as ",filename)
-			else:
-				print("[-]Enter a .png file")
-				sys.exit(1)
+    if password == "no_password_given":
+        password = "no_password_given"
+    embedded_message = password + " " + message
+    secret_image = lsb.hide(image_file, embedded_message)
+    random_number = random.randint(0, 100)
+    filename = "secret" + str(random_number) + ".png"
+    secret_image.save(filename)
+    print("File Saved as ", filename)
 
-		elif args.f and args.x:
-			if not args.p:
-				print("[-]Enter a password")
-				print("[*] If you don't want to supply the password just type apped -p at the end of the query")
-				sys.exit(1)
-			else:
-				message = lsb.reveal(args.f)
-				if (len(re.findall('\\b'+args.p+'\\b',message)) != 0):
-					message = message.replace(args.p,"")
-					print("The secret Message is ",message) 
+def reveal_message(image_file, password):
+    if not password:
+        print("[-] Enter a password")
+        sys.exit(1)
 
-				else:
-					print("Couldn't reveal the Message with the Passcode.")
-						
-		else:
-			print("[-]Error Occured","\n[*]Try Again with the correct syntax.")
-			print("[*]If you are trying to embed a message, try -e flag.")
-			print("[*]If you are trying to extract a message then try -x flag")
-			sys.exit(1)
+    message = lsb.reveal(image_file)
+    if re.findall(f'\\b{password}\\b', message):
+        message = message.replace(password, "")
+        print("The secret Message is ", message)
+    else:
+        print("Couldn't reveal the Message with the Passcode.")
 
-except FileNotFoundError:
-	print("[-]File not Found.")
-	
-except KeyboardInterrupt:
-	print("[-]Keyboard Interrupt.")
+def main():
+    parser = argparse.ArgumentParser(description="Image Steganography\n[+] Use .png files only")
+    parser.add_argument('-e', type=str, help="To embed a message in the image")
+    parser.add_argument('-f', help="To pass .png (Image) file")
+    parser.add_argument('-x', help="To extract the message from the image.", action="store_true")
+    parser.add_argument('-p', help="To put in a password.", nargs="?", const="no_password_given")
+    args = parser.parse_args()
+
+    if args.f and args.e:
+        embed_message(args.f, args.e, args.p)
+    elif args.f and args.x:
+        reveal_message(args.f, args.p)
+    else:
+        print("[-]Error Occured")
+        print("[*]Try Again with the correct syntax.")
+        print("[*]If you are trying to embed a message, try -e flag.")
+        print("[*]If you are trying to extract a message then try -x flag")
+
+if __name__ == "__main__":
+    main()
